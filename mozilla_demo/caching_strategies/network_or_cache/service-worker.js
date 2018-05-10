ae6f9976bc53fd143fe2cd6ad74a879e4a400e33
Omelet <522981616@qq.com>
2018-05-10 15:48:54 +0800

    demo: network_or_cache

--- mozilla_demo/caching_strategies/network_or_cache/service-worker.js
+++ mozilla_demo/caching_strategies/network_or_cache/service-worker.js
@@ -1,26 +1,18 @@
-var CACHE = 'network-or-cache';
+var CACHE = 'network-or-cache'
 
-// On install, cache some resource.
 self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
+  console.log('The service worker is being installed.')
+  evt.waitUntil(precache())
+})
 
-  // Ask the service worker to keep installing until the returning promise
-  // resolves.
-  evt.waitUntil(precache());
-});
-
-// On fetch, use cache but update the entry with the latest contents
-// from the server.
 self.addEventListener('fetch', function(evt) {
   console.log('The service worker is serving the asset.');
-  // Try network and if it fails, go for the cached copy.
   evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
     return fromCache(evt.request);
   }));
 });
 
-// Open a cache and use `addAll()` with an array of assets to add all of them
-// to the cache. Return a promise resolving when all the assets are added.
+
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
@@ -30,28 +22,23 @@ function precache() {
   });
 }
 
-// Time limited network request. If the network fails or the response is not
-// served before timeout, the promise is rejected.
+
 function fromNetwork(request, timeout) {
   return new Promise(function (fulfill, reject) {
-    // Reject in case of timeout.
-    var timeoutId = setTimeout(reject, timeout);
-    // Fulfill in case of success.
+    var timeoutId = setTimeout(reject, timeout)
     fetch(request).then(function (response) {
-      clearTimeout(timeoutId);
-      fulfill(response);
-    // Reject also if network fetch rejects.
-    }, reject);
-  });
+      console.log('get image from network!')
+      clearTimeout(timeoutId)
+      fulfill(response)
+    }, reject)
+  })
 }
 
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
+      console.log('get image from cache storage!')
+      return matching || Promise.reject('no-match')
+    })
+  })
 }
