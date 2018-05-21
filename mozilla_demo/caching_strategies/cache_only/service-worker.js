c62da7cd687faa42414c8c688d652e32bd26d2c9
Omelet <522981616@qq.com>
2018-05-21 15:33:13 +0800

    cache only

---
+++ mozilla_demo/caching_strategies/cache_only/service-worker.js
@@ -0,0 +1,37 @@
+var CACHE = 'cache-only'
+
+var IAMGE_URL = 'https://picsum.photos/200/300/?random'
+
+self.addEventListener('install', function (evt) {
+  console.log('The service worker is being installed')
+
+  evt.waitUntil(precache())
+})
+
+
+self.addEventListener('fetch', function (evt) {
+  console.log('The service worker is serving the asset')
+
+  evt.responseWith(fromCache(evt.request))
+})
+
+
+function precache() {
+  return caches.open(CACHE)
+    .then(function (cache) {
+      return cache.addAll([
+        './index.html',
+        IAMGE_URL
+      ])
+    })
+}
+
+function precache(request) {
+  return caches.open(CACHE)
+    .then(function (cache) {
+      return cache.match(request)
+        .then(function (matching) {
+          return matching || Promise.reject('no-match')
+        })
+    })
+}
