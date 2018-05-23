1c5c4a49c36ffb949717d473adde2d8066824ca5
Omelet <522981616@qq.com>
2018-05-23 15:25:30 +0800

    embedded fallback

--- mozilla_demo/caching_strategies/embedded_fallback/service-worker.js
+++ mozilla_demo/caching_strategies/embedded_fallback/service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-      './controlled.html'
+      './index.html'
     ])
   })
 }
